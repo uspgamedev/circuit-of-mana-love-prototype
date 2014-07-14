5f5c7b8b7778c9fde4c4c24f0a9d3e303248177f
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-07-14 15:54:13 -0300

    Changed views to swap using buttons 1, 2 and 3

--- main.lua
+++ main.lua
@@ -3,11 +3,13 @@ local W, H
 
 local viewnames
 local views
+local current
 
 function love.load ()
   W = love.window.getWidth()
   H = love.window.getHeight()
   viewnames = { 'platformer', 'topdown', 'isometric', 'craft' }
+  current = 1
   views = {}
   love.graphics.setBackgroundColor(100,100,100)
   for _,viewname in ipairs(viewnames) do
@@ -21,18 +23,25 @@ end
 function love.keypressed (key)
   if key == 'escape' then
     love.event.push 'quit'
+  elseif tonumber(key) then
+    current = tonumber(key)
   end
 end
 
 function love.draw ()
   local g = love.graphics
+  -- Draw the divisory line
   g.line(W/2, 0, W/2, H)
-  g.line(0, H/2, W, H/2)
-  for i,viewname in pairs(viewnames) do
-    local view = views[viewname]
+  -- Draw the current view
+  do
+    local view = views[viewnames[current]]
+    view.draw(g, W/2, H)
+  end
+  -- Draw the crafting panel
+  do
     g.push()
-    g.translate((W/2)*((i-1)%2), (H/2)*math.floor((i-1)/2))
-    view.draw(g, W/2, H/2)
+    g.translate(W/2, 0)
+    views.craft.draw(g, W/2, H)
     g.pop()
   end
 end

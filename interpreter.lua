30b39c1c5b2011244cd5f7aec176b218dd7072ef
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-07-16 18:54:54 -0300

    Very ugly fireball

--- interpreter.lua
+++ interpreter.lua
@@ -3,11 +3,11 @@ local interpreter = {}
 
 function interpreter.cast (circuit, nodes)
   for j,slot in ipairs(circuit.layers[1]) do
-    table.insert(slot.manas, { amount = 1 })
+    table.insert(slot.manas, { type = 'substance:mana', amount = 1 })
   end
   for i,layer in ipairs(circuit.layers) do
     for j,slot in ipairs(layer) do
-      if slot.type and slot.to then
+      if #slot.manas > 0 and slot.type and slot.to then
         local to_i, to_j = unpack(slot.to)
         if to_i > i then
           --print("("..i..","..j..") -> ("..to_i..","..to_j..")")
@@ -19,7 +19,7 @@ function interpreter.cast (circuit, nodes)
           local result = node.action(unpack(slot.manas))
           --print "Output:"
           if result then
-            table.foreach(result, print)
+            --table.foreach(result, print)
             table.insert(circuit.layers[to_i][to_j].manas, result)
           else
             --print "<Nothing>"
@@ -29,6 +29,14 @@ function interpreter.cast (circuit, nodes)
       end
     end
   end
+  local result = {}
+  for j,slot in ipairs(circuit.layers[#circuit.layers]) do
+    if slot.manas and #slot.manas > 0 then
+      table.insert(result, unpack(slot.manas))
+    end
+    slot.manas = {}
+  end
+  return result
 end
 
 return interpreter
--- nodes.lua
+++ nodes.lua
@@ -5,18 +5,41 @@ local function accumulator(n)
     total = total + mana.amount
     if total >= n then
       total = 0
-      return { amount = 10 }
+      return { type = 'substance:mana', amount = 10 }
     end
   end
 end
 
+local function projectile (avatars, dt)
+  local hero = avatars[1]
+  local proj = {
+    pos = { hero.pos[1], hero.pos[2]-0.3 },
+    sprite = love.graphics.newImage "assets/fireball00.png"
+  }
+  table.insert(avatars, proj)
+  for i=1,1200 do
+    avatars, dt = coroutine.yield()
+    proj.pos[1] = proj.pos[1] + dt*5
+  end
+  return true
+end
+
 return {
   {
-    action = accumulator(10)
+    name = "Accumulator",
+    action = accumulator(100)
   },
   {
+    name = "Fire",
     action = function (mana)
-      print(mana.amount)
+      mana.type = 'substance:fire'
+      return mana
+    end
+  },
+  {
+    name = "Projectile",
+    action = function (mana)
+      return coroutine.wrap(projectile)
     end
   }
 }

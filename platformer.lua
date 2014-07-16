7bddd7f1606624ceaa9e508c9d560b0b45714125
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-07-16 14:34:55 -0300

    Platformer view ready

--- platformer.lua
+++ platformer.lua
@@ -1,4 +1,38 @@
 
+local avatars = {
+  {
+    pos = {-2, 0},
+    sprite = love.graphics.newImage "assets/chara00.png"
+  },
+  {
+    pos = {2, 0},
+    sprite = love.graphics.newImage "assets/chara01.png"
+  }
+}
+
+local function draw_avatar (graphics, avatar)
+  local x,y = unpack(avatar.pos)
+  local w,h = avatar.sprite:getDimensions()
+  graphics.push()
+  graphics.translate(24*x, 24*y)
+  graphics.draw(
+    avatar.sprite,
+    0, 0, 0, 1, 1, w/2, 0.9*h
+  )
+  graphics.pop()
+  graphics.setColor(255, 255, 255, 255)
+end
+
 function draw (graphics, width, height)
-  graphics.print("Platformer", width/2, height/2)
+  graphics.translate(width/2, height/2)
+  graphics.setColor(115, 130, 105, 255)
+  graphics.rectangle(
+    'fill',
+    -width/4, 0,
+    width/2, height/8
+  )
+  graphics.setColor(255, 255, 255, 255)
+  for _,avatar in ipairs(avatars) do
+    draw_avatar(graphics, avatar)
+  end
 end
